4e01d69c9bb96b9b5f8336fc8f8ed8e07ac99875
Julien Mialon <mialon.julien@gmail.com>
2018-06-14 13:18:51 +0200

    code cleanup

---
+++ src/StormCollectionViews.Samples/Controls/RowContainer.cs
@@ -0,0 +1,17 @@
+﻿using UIKit;
+
+namespace StormCollectionViews
+{
+	internal class RowContainer
+	{
+		public UILayoutGuide Guide { get; }
+
+		public UIView Cell { get; }
+
+		public RowContainer(UILayoutGuide guide, UIView cell)
+		{
+			Guide = guide;
+			Cell = cell;
+		}
+	}
+}
--- src/StormCollectionViews.Samples/Controls/StormCollectionView.cs
+++ src/StormCollectionViews.Samples/Controls/StormCollectionView.cs
@@ -6,19 +6,6 @@ using UIKit;
 
 namespace StormCollectionViews
 {
-	internal class RowContainer
-	{
-		public UILayoutGuide Guide { get; }
-
-		public UIView Cell { get; }
-
-		public RowContainer(UILayoutGuide guide, UIView cell)
-		{
-			Guide = guide;
-			Cell = cell;
-		}
-	}
-
 	public class StormCollectionView : UIScrollView, IStormCollectionView
 	{
 		private const string SELECTOR_BOUNDS_SIZE = "bounds";
